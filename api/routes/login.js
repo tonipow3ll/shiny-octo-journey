9c53d9b074258a7b1eb1c6e250e53511b7082e2e
Toni Powell <tonipow3ll@gmail.com>
2021-06-18 14:53:54 -0400

    Login route working in postman

--- api/routes/login.js
+++ api/routes/login.js
@@ -30,72 +30,23 @@ router.get('/', (req, res) => {
 // module.exports = router;
 // WILL BE the post route for LOGIN
 
-router.post('/', async (req, res) => {
+router.post('/', async(req, res) => {
     const userProf = req.body;
-    // console.log(req.body)
-    // const client = new MongoClient('mongodb+srv://ToniPow3ll:' + process.env.MONGO_PASSWORD + '@cluster0.ssdij.mongodb.net/THETIDES?retryWrites=true&w=majority');
-    // const db = MongoClient.db('db-name');
-    // const users = db.collection('users');
-    
-// User.findOne({ email: userProf.email })
-    User.findOne({ email: userProf.email }).then(() => {
-        // if (!User.email === userProf.email) {
-            // console.log("HELLO")
-            // res.status(200)
-            newUser = {
-                email: req.body.email,
-                // password: req.body.password
-            }
-            // console.log(newUser)
-            User.create(newUser).then((newUser) => {
-                res.json(newUser)
-                res.status(200)
-            }).catch((err) => {
-                res.status(400).json(err)
-            })
-            // client.close();
-            // return callback(err || new WrongUsernameOrPasswordError(email));
-        // } 
-       
-            // console.log("Im getting hung up here, no 200 status")
-            // res.status(200)
-        
-        // bcrypt.compare(User.password, req.body.password, function (err, isValid) {
-        // //     // client.close();
-
-        //     if (req.body.password || !isValid) {
-        //         res.status(400).json(err)
-        //     } else {
-        //         res.status(200)
-        //     }
-
-        //     // return callback(null, {
-        //     //     user_id: user._id.toString(),
-        //     //     nickname: user.nickname,
-        //     //     email: user.email
-            // });
-        });
-    })
-
-    // User.findOne({ email: userProf.email }, function (err, user) {
-    //     if (err || !user) {
-    //         client.close();
-    //         return callback(err || new WrongUsernameOrPasswordError(email));
-    //     }
-    //     bcrypt.compare(password, userProf.password, function (err, isValid) {
-    //         client.close();
-
-    //         if (err || !isValid)
-    //             return callback(err || new WrongUsernameOrPasswordError(email));
-
-    //         return callback(null, {
-    //             user_id: user._id.toString(),
-    //             nickname: user.nickname,
-    //             email: user.email
-    //         });
-    //     });
-    // });
-// })
+    const newUser = {
+        email: req.body.email
+    }
+    let user = await User.findOne({email: userProf.email})
+    if (user) {
+        console.log('user found')
+        res.status(200)
+        res.json(user)
+    } else {
+        user = await User.create(newUser)
+        console.log('user created')
+        res.status(200)
+        res.json(user)
+    }
+})
 
 
 
