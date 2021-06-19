abe145db8e225505851ff8f151337a70bf99ab81
Toni Powell <tonipow3ll@gmail.com>
2021-06-19 08:05:23 -0400

    react-router-dom issue in compling, added noimplicitany

--- client/src/auth/auth0-provider-with-history.tsx
+++ client/src/auth/auth0-provider-with-history.tsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { History } from 'history';
 
+
 const Auth0ProviderWithHistory:React.FC = ({ children }):JSX.Element => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN || "WRONG";
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || "WRONG";
